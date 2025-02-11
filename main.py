5775cde05db51d18815737645f05abd19dff3559
Josh Alling <joshralling@gmail.com>
2025-02-10 20:55:35 -0800

    move player

--- main.py
+++ main.py
@@ -10,11 +10,10 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill((0, 0, 0))
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    player.draw(screen)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -24,8 +23,14 @@ def main():
             if event.type == pygame.QUIT:
                 exit()
 
+        player.update(dt)
+        screen.fill((0, 0, 0))
+
+        player.draw(screen)
+
         pygame.display.flip()
-        clock.tick(60)
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
